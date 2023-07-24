3f70eef900a380984f890e32d25cbb9af4feaba9
SiziphiweD <siziphiwes13@gmail.com>
2023-07-24 18:48:26 +0000

    Task-0

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,8 +1,6 @@
 // 0-main.js
 
-// Import the functions from the 0-constants.js
-const{ taskFirst, taskNext }require('./0-constants.js');
+// Import the functions from the 0-constan.js
+import { taskFirst, taskNext } from './0-constants.js';
 
-// Execute the functions and log the output
 console.log(`${taskFirst()} ${taskNext()}`);
-
