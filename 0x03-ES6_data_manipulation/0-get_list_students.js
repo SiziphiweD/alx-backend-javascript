d2b5e03e45ee9ab1fb07a8e6410335ed27014055
SiziphiweD <siziphiwes13@gmail.com>
2023-07-31 19:50:15 +0000

    get_list_students

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,15 @@
+// File: 0x03-ES6_data_manipulation/0-get_list_students.js
+
+// Define the getListStudents function
+function getListStudents() {
+  // Create and return an array of objects
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
+}
+
+// Export the function to make it accessible from other files
+export default getListStudents;
+
