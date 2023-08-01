b8923097da631788650c28205c9ca5fe307aa188
Siziphiwe <siziphiwes13@gmail.com>
2023-08-01 21:07:49 +0000

    GetStudentList

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -6,6 +6,4 @@ function getListStudents() {
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' }
   ];
-}
 
-export default getListStudents;
--- 0x03-ES6_data_manipulation/0-main.js
+++
@@ -1,6 +0,0 @@
-// 0-main.js
-
-import getListStudents from "./0-get_list_students.js";
-
-console.log(getListStudents());
-
